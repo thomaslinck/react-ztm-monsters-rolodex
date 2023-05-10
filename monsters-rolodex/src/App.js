e9b118c6c3f8ab4afa4185d419faba248c41d147
thomaslinck <thomaspegg.linck@gmail.com>
2023-05-10 16:22:58 +0700

    create search/filter

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -6,29 +6,47 @@ class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			monsters: [],
+			allMonsters: [],
+			monstersToDisplay: [],
 		};
-		this.url = 'https://jsonplaceholder.typicode.com/users';
+		this.monstersSource = 'https://jsonplaceholder.typicode.com/users';
 	}
 	componentDidMount() {
 		// this method is called in the first time the component is rendered
-		fetch(this.url)
+		fetch(this.monstersSource)
 			.then((response) => response.json())
-			.then((users) =>
+			.then((responseInJson) =>
 				this.setState(
-					() => {
-						return {
-							monsters: users,
-						};
+					{
+						allMonsters: responseInJson,
+						monstersToDisplay: responseInJson,
 					},
 					() => console.log(this.state)
 				)
-			);
+			)
+			.catch((error) => console.error('Monsters API call failed'));
 	}
 	render() {
 		return (
 			<div className="App">
-				{this.state.monsters.map((m) => (
+				<input
+					className="search-box"
+					type="search"
+					placeholder="search monsters"
+					onChange={(event) => {
+						this.setState({
+							monstersToDisplay: this.state.allMonsters.filter(
+								(monster) =>
+									monster.name
+										.toLowerCase()
+										.includes(
+											event.target.value.toLowerCase()
+										)
+							),
+						});
+					}}
+				/>
+				{this.state.monstersToDisplay.map((m) => (
 					<h1 key={m.id}>{m.name}</h1>
 				))}
 			</div>
