8d1e081cbe399bc3de22513de39c241c29388ae2
thomaslinck <thomaspegg.linck@gmail.com>
2023-05-10 16:56:29 +0700

    extracted anonymous functions into methods

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,16 +1,16 @@
 import { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			allMonsters: [],
-			monstersToDisplay: [],
+			monsters: [],
+			searchField: '',
 		};
 		this.monstersSource = 'https://jsonplaceholder.typicode.com/users';
 	}
+
 	componentDidMount() {
 		// this method is called in the first time the component is rendered
 		fetch(this.monstersSource)
@@ -18,37 +18,45 @@ class App extends Component {
 			.then((responseInJson) =>
 				this.setState(
 					{
-						allMonsters: responseInJson,
-						monstersToDisplay: responseInJson,
+						monsters: responseInJson,
 					},
 					() => console.log(this.state)
 				)
 			)
 			.catch((error) => console.error('Monsters API call failed'));
 	}
+	componentDidUpdate() {
+		console.log(this.state.monsters);
+		console.log(this.state.searchField);
+	}
+
+	onSearchBoxChange = (event) => {
+		this.setState({
+			searchField: event.target.value.toLowerCase(),
+		});
+	};
+
+	byMonsterThatIncludeSearchText = (monster) => {
+		return monster.name.toLowerCase().includes(this.state.searchField);
+	};
+
+	createMonsterTag(monster) {
+		return <h1 key={monster.id}>{monster.name}</h1>;
+	}
 	render() {
+		const { monsters } = this.state;
+
 		return (
 			<div className="App">
 				<input
 					className="search-box"
 					type="search"
 					placeholder="search monsters"
-					onChange={(event) => {
-						this.setState({
-							monstersToDisplay: this.state.allMonsters.filter(
-								(monster) =>
-									monster.name
-										.toLowerCase()
-										.includes(
-											event.target.value.toLowerCase()
-										)
-							),
-						});
-					}}
+					onChange={this.onSearchBoxChange}
 				/>
-				{this.state.monstersToDisplay.map((m) => (
-					<h1 key={m.id}>{m.name}</h1>
-				))}
+				{monsters
+					.filter(this.byMonsterThatIncludeSearchText)
+					.map(this.createMonsterTag)}
 			</div>
 		);
 	}
